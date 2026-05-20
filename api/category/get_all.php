7c2c1c5082eb71ac082a14fa0fcfb857dfa3aa3f
manju9711 <manjuterasha97@gmail.com>
2026-05-20 16:12:09 +0530

    manju new chnages commit

--- api/category/get_all.php
+++ api/category/get_all.php
@@ -1,11 +1,10 @@
 <?php
-// 🔥 CORS HEADERS
+
 header("Content-Type: application/json");
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 header("Access-Control-Allow-Methods: POST, OPTIONS");
 
-// 🔥 PREFLIGHT
 if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
     http_response_code(200);
     exit();
@@ -16,12 +15,19 @@ include __DIR__ . '/../../config/db.php';
 $company_id = $_GET['company_id'] ?? 0;
 
 if (!$company_id) {
-    echo json_encode(["status"=>false,"message"=>"company_id required"]);
+    echo json_encode([
+        "status"=>false,
+        "message"=>"company_id required"
+    ]);
     exit;
 }
 
-$result = mysqli_query($conn, "SELECT * FROM categories 
-WHERE company_id='$company_id' AND is_deleted=0");
+$result = mysqli_query($conn, "
+SELECT * FROM categories 
+WHERE company_id='$company_id'
+AND is_deleted=0
+
+");
 
 $data = [];
 
@@ -29,5 +35,8 @@ while ($row = mysqli_fetch_assoc($result)) {
     $data[] = $row;
 }
 
-echo json_encode(["status"=>true,"data"=>$data]);
+echo json_encode([
+    "status"=>true,
+    "data"=>$data
+]);
 ?>
