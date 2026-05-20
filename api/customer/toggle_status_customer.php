247177afaf950e49ed10f323bfb9629cbe8eee53
manju9711 <manjuterasha97@gmail.com>
2026-05-20 16:44:04 +0530

    manju new chnages commit

---
+++ api/customer/toggle_status_customer.php
@@ -0,0 +1,52 @@
+<?php
+
+header("Content-Type: application/json");
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+header("Access-Control-Allow-Methods: POST, OPTIONS");
+
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
+
+include __DIR__ . '/../../config/db.php';
+$data = json_decode(file_get_contents("php://input"), true);
+
+$id = intval($data['id'] ?? 0);
+$status = $data['status'] ?? '';
+
+if (!$id || !$status) {
+
+    echo json_encode([
+        "success" => false,
+        "message" => "Invalid data"
+    ]);
+
+    exit;
+}
+
+$stmt = $conn->prepare("
+UPDATE customers
+SET status=?
+WHERE id=?
+");
+
+$stmt->bind_param("si", $status, $id);
+
+if ($stmt->execute()) {
+
+    echo json_encode([
+        "success" => true,
+        "message" => "Status updated"
+    ]);
+
+} else {
+
+    echo json_encode([
+        "success" => false,
+        "message" => $stmt->error
+    ]);
+
+}
+?>
