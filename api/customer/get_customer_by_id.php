77c7a50d5be5688346e9740c247e3496ff291df5
suryapcs <suryapcs91@gmail.com>
2026-06-05 11:26:07 +0530

    happy birthday manju akka

--- api/customer/get_customer_by_id.php
+++ api/customer/get_customer_by_id.php
@@ -3,21 +3,30 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: Content-Type");
 header("Access-Control-Allow-Methods: POST, OPTIONS");
 
-// Handle preflight request
 if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
     http_response_code(200);
     exit();
 }
+
 include "../../config/db.php";
 
 $data = json_decode(file_get_contents("php://input"), true);
 $id = intval($data['id']);
 
-$res = $conn->query("
-SELECT * FROM customers WHERE id='$id' LIMIT 1
-");
+$sql = "
+SELECT
+    c.*,
+    co.gstin as company_gstin
+FROM customers c
+LEFT JOIN companies co
+    ON c.company_id = co.id
+WHERE c.id = '$id'
+LIMIT 1
+";
+
+$res = $conn->query($sql);
 
 echo json_encode([
-    "status"=>true,
-    "data"=>$res->fetch_assoc()
+    "status" => true,
+    "data" => $res->fetch_assoc()
 ]);
