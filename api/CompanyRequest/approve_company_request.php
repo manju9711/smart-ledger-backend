07ef87c54e10b905eeb9e1e1e376fbfd89615910
manju9711 <manjuterasha97@gmail.com>
2026-06-24 16:35:29 +0530

    manju new commit

--- api/CompanyRequest/approve_company_request.php
+++ api/CompanyRequest/approve_company_request.php
@@ -1,16 +1,23 @@
 <?php
 
 header("Access-Control-Allow-Origin: *");
-header("Content-Type: application/json");
+header("Access-Control-Allow-Headers: Content-Type");
+header("Access-Control-Allow-Methods: POST, OPTIONS");
 
+// Handle preflight request
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
 include "../../config/db.php";
+
 
 $data = json_decode(
     file_get_contents("php://input"),
     true
 );
 
-$id = intval($data['id'] ?? 0);
+$id = intval($data['request_id'] ?? 0);
 
 $res = mysqli_query($conn,"
 SELECT *
