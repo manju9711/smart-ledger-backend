d2f3c0e61cca71baea64201085d59f417a225597
manju9711 <manjuterasha97@gmail.com>
2026-06-25 12:19:51 +0530

    manju commit

--- api/product/get.php
+++ api/product/get.php
@@ -14,11 +14,11 @@ include "../../config/db.php";
 
 $company_id = intval($_GET['company_id'] ?? 0);
 
-if(!$company_id){
+if (!$company_id) {
 
     echo json_encode([
-        "status"=>true,
-        "data"=>[]
+        "status" => true,
+        "data" => []
     ]);
     exit;
 }
@@ -27,15 +27,21 @@ $result = mysqli_query($conn, "
 
 SELECT
     p.*,
-    c.name as category_name
+    c.name AS category_name,
+    comp.company_name,
+    comp.gstin AS company_gstin,
+    comp.gst_type
 
 FROM products p
 
 LEFT JOIN categories c
 ON p.category_id = c.id
 
-WHERE p.company_id='$company_id'
-AND p.is_deleted=0
+LEFT JOIN companies comp
+ON p.company_id = comp.id
+
+WHERE p.company_id = '$company_id'
+AND p.is_deleted = 0
 
 ORDER BY p.id DESC
 
@@ -43,13 +49,13 @@ ORDER BY p.id DESC
 
 $data = [];
 
-while($row = mysqli_fetch_assoc($result)){
+while ($row = mysqli_fetch_assoc($result)) {
 
     $data[] = $row;
 
 }
 
 echo json_encode([
-    "status"=>true,
-    "data"=>$data
+    "status" => true,
+    "data" => $data
 ]);
